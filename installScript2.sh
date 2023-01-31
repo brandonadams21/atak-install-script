fa54f76fef383100c28a15c6935124383c5bfe5e
Brandon <adamsbrandon02@gmail.com>
2023-01-31 15:24:11 -0600

    Initial Commit

--- installScript2.sh
+++ installScript2.sh
@@ -65,31 +65,31 @@ done
 
 #Handle transfer methods
 while true; do
-# echo "How do you want to transfer your TAK Server ZIP File?"
-# echo "A: Google Drive Transfer"
-# echo "B: Transfer from Local Machine"
-# echo ""
+echo "How do you want to transfer your TAK Server ZIP File?"
+echo "A: Google Drive Transfer"
+echo "B: Transfer from Local Machine"
+echo ""
 read transferoption
         case $transferoption in
-                # [Aa]* )
-                #         echo "*****************************************"
-                #         echo "Using Google Drive Method"
-                #         echo "*****************************************"
-                #         echo ""
-                #         echo "WHAT IS YOUR FILE ID ON GOOGLE DRIVE?"
-                #         echo "(Right click > Get Link > Allow Sharing to anyone with link > Open share link > 'https://drive.google.com/file/d/<YOUR_FILE_ID_IS_HERE>/view?usp=sharing')"
-                #         read FILE_ID
+                [Aa]* )
+                        echo "*****************************************"
+                        echo "Using Google Drive Method"
+                        echo "*****************************************"
+                        echo ""
+                        echo "WHAT IS YOUR FILE ID ON GOOGLE DRIVE?"
+                        echo "(Right click > Get Link > Allow Sharing to anyone with link > Open share link > 'https://drive.google.com/file/d/<YOUR_FILE_ID_IS_HERE>/view?usp=sharing')"
+                        read FILE_ID
 
-                #         echo "WHAT IS YOUR FILE NAME?"
-                #         echo "(ex: takserver-docker-4.7-RELEASE-20.zip)"
-                #         read FILE_NAME
+                        echo "WHAT IS YOUR FILE NAME?"
+                        echo "(ex: takserver-docker-4.7-RELEASE-20.zip)"
+                        read FILE_NAME
                         
-                #         cd ~/tak-server
-                #         sudo wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILE_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p'
-                #         sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=t&id=$FILE_ID" -O $FILE_NAME
-                #         rm -rf /tmp/cookies.txt
-                #         break
-                # ;;
+                        cd ~/tak-server
+                        sudo wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=$FILE_ID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p'
+                        sudo wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=t&id=$FILE_ID" -O $FILE_NAME
+                        rm -rf /tmp/cookies.txt
+                        break
+                ;;
                 [Bb]* )
                         echo "*****************************************"
                         echo "Transfering from Local Machine"
